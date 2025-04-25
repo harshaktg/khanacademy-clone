49f4312d8e7506f72a307223dcb41f8115323380
HarshaVardhan <harshaktg@gmail.com>
2025-04-26 01:20:45 +0530

    refactor(course-accordion): simplify layout structure
    - Remove unnecessary wrapper divs\n- Improve responsive icon sizing for desktop\n- Maintain grid layout with cleaner structure\n- Fix spacing and margins for list items\n- Use template tag for conditional rendering

--- src/components/Courses.vue
+++ src/components/Courses.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { computed } from 'vue';
+import CourseAccordion from './CourseAccordion.vue';
 
 interface CourseItem {
     name: string;
@@ -536,72 +537,17 @@ const getCourseItems = (course: Course) => {
             <div v-for="[sectionId, sectionCourses] in coursesBySection" :key="sectionId"
                 class="flex flex-col relative items-stretch">
                 <!-- Course Container -->
-                <div v-for="course in sectionCourses" :key="course.name"
-                    class="flex flex-col relative items-stretch border-b border-[#21242c52] lg:grid lg:grid-cols-[1fr_1fr]"
-                    :class="course.class">
-                    <!-- Course Header -->
-                    <button aria-expanded="true" type="button" aria-disabled="false"
-                        class="font-bold grid grid-cols-[auto_1fr_auto] items-center bg-transparent leading-5 overflow-visible py-3 text-left section-header">
-                        <img :src="course.icon" width="45" height="45" class="rounded-full mr-3 w-[34px] h-[34px]"
-                            :alt="course.name">
-                        {{ course.name }}
-                        <span class="caret-down"></span>
-                    </button>
-
-                    <!-- Primary List -->
-                    <ul v-if="getCourseItems(course).primary.length"
-                        class="ml-[46px] lg:ml-[57px] lg:-mt-[5px] lg:mb-3">
-                        <li v-for="item in getCourseItems(course).primary" :key="item.name"
-                            class="hover:text-primary hover:underline hover:cursor-pointer">
-                            <a class="block items-center leading-5 item-list" :class="item.class" :href="item.href">
-                                <span>{{ item.name }}</span>
-                            </a>
-                        </li>
-                    </ul>
-
-                    <!-- Secondary List -->
-                    <ul v-if="getCourseItems(course).secondary.length"
-                        class="ml-[46px] mb-2 lg:ml-6 lg:-mt-[5px] lg:mb-3">
-                        <li v-for="item in getCourseItems(course).secondary" :key="item.name"
-                            class="hover:text-primary hover:underline hover:cursor-pointer">
-                            <a class="block items-center leading-5 item-list" :class="item.class" :href="item.href">
-                                <span>{{ item.name }}</span>
-                            </a>
-                        </li>
-                    </ul>
-                </div>
+                <CourseAccordion v-for="course in sectionCourses" :key="course.name" :course="course" />
             </div>
         </div>
     </nav>
 </template>
 
 <style scoped>
-.caret-down {
-    @apply w-6 h-6 inline-block align-text-bottom;
-    mask-size: 100%;
-    mask-repeat: no-repeat;
-    mask-position: center;
-    mask-image: url(https://cdn.kastatic.org/genwebpack/prod/en/images/188ef46fb20241ed-caret-down-bold.svg);
-    background-color: #21242c;
-    transition: -ms-transform .5s, -webkit-transform .5s, transform .5s;
-    transform: rotate(-180deg);
-}
-
-.item-list {
-    padding-block: 8px;
-}
-
 @media screen and (min-width: 1024px) {
     .grid-container {
         grid-template-columns: repeat(2, 1fr);
         column-gap: 100px;
-    }
-
-    .section-header {
-        @apply text-xl;
-        padding-block: 16px;
-        grid-column-start: 1;
-        grid-column-end: 3;
     }
 }
 </style>
